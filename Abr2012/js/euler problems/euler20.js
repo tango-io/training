9711c7803e5b4c38d4a90391031295b046c6ac7e
VictorChapa <victor.chapa@tangosource.com>
2012-04-12 16:22:32 -0500

    Completed exercise saving the data into vector.

--- Abr2012/js/euler problems/euler20.js
+++ Abr2012/js/euler problems/euler20.js
@@ -8,3 +8,21 @@
 // Find the sum of the digits in the number 100!
 //
 // https://projecteuler.net/problem=20
+//
+
+function EulerProblem20(number){
+  var sum = 0, fac = 1, v = [];
+  for (cont = 2; cont <= number; cont++){
+    fac = fac * cont;
+  }
+  chain = fac.toString();
+  for (cont = 0; cont <= chain.length-1; cont++){
+    v[cont] = parseInt(chain.charAt(cont));
+    sum = sum + v[cont];
+  }
+  console.log("The factorial of " + number + "! is : " + chain + ".");
+  console.log("The sum of each elements of " + number + "! is: " + sum + ".");
+}
+
+EulerProblem20(10);
+
