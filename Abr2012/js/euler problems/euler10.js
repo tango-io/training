977c70ad6a742eda27c2fde170cf2adbd0facc56
Oscar Vargas <oscar.vargas@tangosource.com>
2012-04-12 16:19:27 -0500

    the first version of the problen, it does work but is slowly

--- Abr2012/js/euler problems/euler10.js
+++ Abr2012/js/euler problems/euler10.js
@@ -4,3 +4,24 @@
 // Find the sum of all the primes below two million.
 
 // https://projecteuler.net/problem=10
+
+function primos(q){ 
+  var p=2,  
+      sum=0;
+  for (i = p; i <= q ; i++){ 
+    console.log(i);
+    var count = 2; 
+    for ( j = 2; j<i ; j++){  
+      if (i%j == 0){ 
+        count++; 
+        break; 
+      } 
+    } 
+    if (count == 2){ 
+    sum=sum+i 
+    } 
+  } 
+  console.log(sum)
+}
+
+primos(10);
