6e9cb843ce349247c673a6057afaddb1a1252d58
Oscar Vargas <oscar.vargas@tangosource.com>
2012-04-12 19:11:40 -0500

    second

--- Abr2012/js/euler problems/euler10.js
+++ Abr2012/js/euler problems/euler10.js
@@ -5,23 +5,44 @@
 
 // https://projecteuler.net/problem=10
 
-function primos(q){ 
-  var p=2,  
-      sum=0;
-  for (i = p; i <= q ; i++){ 
-    console.log(i);
-    var count = 2; 
-    for ( j = 2; j<i ; j++){  
-      if (i%j == 0){ 
-        count++; 
-        break; 
-      } 
-    } 
-    if (count == 2){ 
-    sum=sum+i 
-    } 
-  } 
-  console.log(sum)
+//function primos(q){ 
+  //var p=2,  
+      //sum=0;
+  //for (i = p; i <= q ; i++){ 
+    //console.log(i);
+    //var count = 2; 
+    //for ( j = 2; j<i ; j++){  
+      //if (i%j == 0){ 
+        //count++; 
+        //break; 
+      //} 
+    //} 
+    //if (count == 2){ 
+    //sum=sum+i 
+    //} 
+  //} 
+  //console.log(sum)
+//}
+
+//primos(10);
+//
+
+var vec=[],
+    sum=0;
+
+function loop(ban){
+  for (var i=0; i<ban; i++) vec[i]=i+1;
+  //console.log(vec);
+  for (var d=4; d<=ban; d=d+2) vec[d-1]=0; vec[0]=0;
+  //console.log(vec);
+  for (var t=6; t<=ban; t=t+3) vec[t-1]=0;
+  //console.log(vec);
+  for (var c=10; c<=ban; c=c+5) vec[c-1]=0;
+  //console.log(vec);
+  for (var r=14; r<=ban; r=r+7) vec[r-1]=0;
+  //console.log(vec);
+  for (var x=1; x<ban; x++) sum=sum+vec[x];
 }
 
-primos(10);
+loop(30);
+console.log(sum);
