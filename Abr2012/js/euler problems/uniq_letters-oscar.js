359cc34b89f0970d8c8aa6185a437c0f17fc804d
Oscar Vargas <oscar.vargas@tangosource.com>
2012-04-13 14:04:59 -0500

    first try

--- Abr2012/js/euler problems/uniq_letters-oscar.js
+++ Abr2012/js/euler problems/uniq_letters-oscar.js
@@ -30,3 +30,47 @@
 //       Y :: 2
 //       ------
 //       19 uniq letters
+//
+//declaramos la cadena y un vector par almacenar la cadena completa ya spliteada
+var str='Le dije a mi mamá que si tengo muerte cerebral y vivo a base de maquinas mejor me desconectara. Ella se levantó y desconectó el internet',
+    con=0,
+    cont=0,
+    vec=[];
+
+function count(arr2){
+  for (var j=0;j<=arr2.length;j++){
+    if ((arr2[j]==arr2[j+1]) && (arr2[j]!=arr2.length)){
+      con=con+1;
+    }else{
+      cont=cont+1;
+      con=con+1;
+      console.log(arr2[j] +' :: '+ con);
+      con=0;
+    }
+  }
+  console.log(cont +' uniq letters')
+}    
+function clean(arr){
+  for (var i=0;i<=arr.length;i++){
+    if (arr[i]==" " || arr[i]=="," || arr[i]=="."){
+      delete arr[i];
+    }
+  }
+  change(arr);
+}
+
+//function change(a, con){
+  //for (var i; i==a.lenght;i++){
+    //b=a[i].toString()
+    //b=b.replace('Á','A');
+    //b=b.replace('É','E');
+    //b=b.replace('Í','I');
+    //b=b.replace('Ó','O');
+    //b=b.replace('Ú','U');
+    //con=con+b;
+  //}
+    //console.log(con);
+//}
+
+vec=str.toUpperCase().split("").sort();
+clean(vec);
