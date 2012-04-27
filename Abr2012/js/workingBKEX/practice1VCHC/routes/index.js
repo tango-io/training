a37cf86d44bff55b68ea3a9604442248eb995b7e
VictorChapa <victor.chapa@tangosource.com>
2012-04-26 20:34:49 -0500

    Added 2 views and 2 routes in Express.

--- Abr2012/js/workingBKEX/practice1VCHC/routes/index.js
+++ Abr2012/js/workingBKEX/practice1VCHC/routes/index.js
@@ -1,8 +1,15 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Eddity' })
+};
+
+/*
+ * GET edite page.
+ */
+
+exports.edit = function(req, res){
+  res.render('edit', { title: 'Edit' })
 };
