0a2ee2c30f09af9c9c5db2d56fac95d66b428b54
Oscar Vargas <oscar.vargas@tangosource.com>
2012-04-27 12:14:40 -0500

    created the view and the libs needs for the web

--- Abr2012/js/backboneoscar/test/app.js
+++ Abr2012/js/backboneoscar/test/app.js
@@ -30,7 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
+app.get ('/edit',routes.edit);
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
