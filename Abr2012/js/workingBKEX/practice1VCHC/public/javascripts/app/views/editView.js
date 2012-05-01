ca1d3a263b1436069f895af7f5c5d05e74b0ef12
VictorChapa <victor.chapa@tangosource.com>
2012-05-01 12:56:47 -0500

    Added title editable in edit.

--- Abr2012/js/workingBKEX/practice1VCHC/public/javascripts/app/views/editView.js
+++ Abr2012/js/workingBKEX/practice1VCHC/public/javascripts/app/views/editView.js
@@ -1,10 +1,39 @@
 Editty.View.Edit = Backbone.View.extend({
   el: 'body',
-
+  
+  events: {
+    'click #title'    : 'edittitle', 
+    'click #content'  : 'editcontent',
+    'keydown #incont' : 'puttitle'
+  
+  },
+  
   initialize: function(){
     $('.nav').append('<li><a>'+window.location.href+'</a></li>');
+    this.conta = 0;
   },
 
   render: function(){
+  },
+  
+  edittitle: function(){
+    this.conta = this.conta + 1;
+    if (this.conta == 1){
+      var titlesaver = $('h1').text();
+      $('h1').remove();
+      $('#title').append('<input id="incont" placeholder="Write here" maxlength="25"></input>');
+    }
+  },
+  
+  puttitle: function(){
+    var keycapture = $('#incont').val();
+    if ((event.keyCode == 13) && (keycapture != '')){
+      $('#title').append('<h1>'+ keycapture +'</h1>');
+      $('#incont').remove();
+    }
+  },
+
+  editcontent: function(){
+  
   }
 });
