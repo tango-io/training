ac902f3cabdeff1b91000336414bf0ad7baba525
Oscar Vargas <oscar.vargas@tangosource.com>
2012-05-02 13:52:11 -0500

    checked the routs

--- Abr2012/js/backboneoscar/test/routes/index.js
+++ Abr2012/js/backboneoscar/test/routes/index.js
@@ -6,6 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Editty' })
 };
+
 exports.edit = function(req, res){
   var id = req.params.id;
   redis.get ('data', function(err, data){
