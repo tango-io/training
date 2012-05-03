d40065783c22e4e3c9dbfc6b7cbb50b516921620
Oscar Vargas <oscar.vargas@tangosource.com>
2012-05-02 19:41:41 -0500

    fixed the edit view

--- Abr2012/js/backboneoscar/test/public/javascripts/app/views/editView.js
+++ Abr2012/js/backboneoscar/test/public/javascripts/app/views/editView.js
@@ -2,9 +2,11 @@ Editty.View.Edit = Backbone.View.extend({
   el: 'body',
 
   events:{
-    'dblclick #titulo'    : 'editTitle',
-    'dblclick #main_text' : 'editText',
-    'input #titleChange'     : 'test'
+    'dblclick #titulo'       : 'editTitle',
+    'dblclick #main_text'    : 'editText',
+    'input #titleChange'     : 'test',
+    'blur #titleChange'      : 'focusOutHead',
+    'blur #contenChange' : 'focusOutCont'
 
   },
   
@@ -14,6 +16,21 @@ Editty.View.Edit = Backbone.View.extend({
 
   initialize: function(){
     $('.nav').append('<li><a>'+window.location.href+'</a></li>');
+  },
+
+  focusOutHead:function(){
+     var aux3 = $('#titleChange').val();
+     $('#titleChange').remove();     
+     $('.header').append('<h1 id="titulo"></h1>');
+     $('#titulo').text(aux3);
+  },
+
+  focusOutCont:function(){
+     var aux4 = $('#contenChange').val();
+     console.log(aux4);
+     $('#contenChange').remove();     
+     $('.conten_web').append('<p id="main_text"></p>');
+     $('#main_text').text(aux4);
   },
 
   editTitle:function(){
