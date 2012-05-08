e2cec5d1d9012ba9d05d3ccc271aa4bdc2d1aa92
Oscar Vargas <oscar.vargas@tangosource.com>
2012-05-08 12:03:52 -0500

    fixed the show view

--- Abr2012/js/backboneoscar/test/public/javascripts/app/views/showView.js
+++ Abr2012/js/backboneoscar/test/public/javascripts/app/views/showView.js
@@ -5,6 +5,17 @@ Editty.View.Show = Backbone.View.extend({
   },
 
   initialize: function(){
+    var self = this;
+    $.ajax({
+      type: 'POST',
+      url: "/getData",
+      data: {id:1} 
+    }).done(function (data){
+      var users = JSON.parse(data);
+          self.user2 = users; 
+          $('#head').text(users.title);
+          $('#mainText').text(users.contenText);
+    });
   },
 
   render: function(){
