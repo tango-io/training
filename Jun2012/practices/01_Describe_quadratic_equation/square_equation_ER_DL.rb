cd529d641d7ff0c293fa9d7cde8791ecaf942531
Daniel Gaytan <dgaytan6@gmail.com>
2012-06-12 00:00:21 -0500

    Added practices folder

---
+++ Jun2012/practices/01_Describe_quadratic_equation/square_equation_ER_DL.rb
@@ -0,0 +1,15 @@
+describe SecondGradeEcuation do
+  it "Should have 1 variable at least" do
+    # Code here
+  end
+
+  it "should have the variable with a maximun exponent to 2" do
+    # Code here
+  end
+
+  context "When a constant is multiplying a variable" do
+    it "should has the constant different to 0" do
+      # Code here
+    end
+  end
+end
---
+++ Jun2012/practices/01_Describe_quadratic_equation/square_equation_NG_AE.js
@@ -0,0 +1,23 @@
+describe("Second grade ecuation", function(){
+
+    it("Should have an unknown variable", function(){});
+
+    it("Fisrt term should be square", function(){});
+
+    it("Should have 3 constants", function(){});
+
+    it("Should be equal to zero", function(){});
+
+    it("First constant should not be zero", function(){});
+
+});
+
+
+describe('QuadraticEquationSolver', function(){
+  it('should receive a valid quadratic equation')
+  it('should give the value of x')
+})
+
+describe('ValidQuadraticEquation', function(){
+  it('should validate the constants')
+})
